427d37d370fefd65be82a6dbd0c5ed95c89b9348
dhruvods <ods60521@gmail.com>
2024-10-03 23:56:25 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -111,11 +111,11 @@ SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/odsmusichub"
 )  # Example:- https://t.me/VIP_CREATORS
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/odsmusichub"
+    "SUPPORT_GROUP", "https://t.me/oldskoolgc"
 )  # Example:- https://t.me/TG_FRIENDSS
 
 SUPPORT_CHAT = getenv(
-    "SUPPORT_GROUP", "https://t.me/odsmusichub"
+    "SUPPORT_GROUP", "https://t.me/oldskoolgc"
 )  # Example:- https://t.me/TG_FRIENDSS
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
